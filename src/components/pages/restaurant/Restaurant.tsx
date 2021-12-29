c71d9d7ae8f0ae60af80b5d644f1b75de712a2c3
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-12-29 22:40:18 +0300

    modify image Restaurant

--- src/components/pages/restaurant/Restaurant.tsx
+++ src/components/pages/restaurant/Restaurant.tsx
@@ -2,6 +2,12 @@ import { CardNumber } from '../../common/cardNumber/CardNumber';
 import { HeadingBlock } from '../../common/heading-block/HeadingBlock';
 import { Reservation } from '../../common/reservation/Reservation';
 import { BlockTitle } from '../../common/title/BlockTitle';
+import Wedding from '../../../img/restaurant/wedding.jpg';
+import Anniversary from '../../../img/restaurant/anniversary.jpg';
+import OfficeParties from '../../../img/restaurant/office-parties.jpg';
+import Graduation from '../../../img/restaurant/graduation.jpg';
+import Birthday from '../../../img/restaurant/birthday.jpg';
+import SlideFourchette from '../../../img/restaurant/slide-fourchette.jpg';
 import './restaurant.scss';
 
 const title = {
@@ -19,37 +25,37 @@ const eventsRes = [
   {
     title: '«Свадьбы»',
     desc: 'Мы верим в то, что браки заключаются на небесах и помогаем каждой паре воплотить свои мечты',
-    url: 'https://n1s1.hsmedia.ru/07/a6/16/07a616194e89c565b4b21adfbe999a79/728x542_1_872b4f20ec7bceb08b3a9c011e59a10d@1000x745_0xac120003_9936791021594995638.gif',
+    url: Wedding,
     cost: 3300,
   },
   {
     title: '«Юбилеи, Дни рождения»',
     desc: 'Бла бла',
-    url: 'https://www.krasnayagorka.ru/sites/default/files/uk.jpg',
+    url: Anniversary,
     cost: 3800,
   },
   {
     title: '«Корпоративы»',
     desc: 'бла бла ',
-    url: 'https://catering-muscat.ru/img/blog/blog_ny-3.jpg',
+    url: OfficeParties,
     cost: 2300,
   },
   {
     title: '«Выпускные»',
     desc: 'бла бла',
-    url: 'https://www.m24.ru/b/d/nBkSUhL2hFMgnM23J76BvMKnxdDs95C-miTNnuWR9mOBdDebBizCnTY8qdJf6ReJ58vU9meMMok3Ee2nhSR6ISeO9G1N_wjJ=fbqx8U4cUBJEDRDt5CLc4g.jpg',
+    url: Graduation,
     cost: 2300,
   },
   {
     title: '«Детские праздники»',
     desc: 'бла бла',
-    url: 'https://mama.ua/media/points/photos/2017/05/dr.jpg',
+    url: Birthday,
     cost: 2300,
   },
   {
     title: '«Фуршеты, презентации»',
     desc: 'Бла Бла',
-    url: 'https://www.barin-catering.com.ua/data/_uploaded/image/Fourchette/slide-fourchette-1.jpg',
+    url: SlideFourchette,
     cost: 2300,
   },
 ];
