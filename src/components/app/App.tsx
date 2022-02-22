1bf7615940f7f013de8848341f0e99a85b593439
Vladimir Liankevich <vladimirlyn@gmail.com>
2022-02-22 18:09:41 +0300

    change socials links

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,11 +6,26 @@ import { Routers } from '../../route/Routers';
 import { HashRouter } from 'react-router-dom';
 
 const socials = [
-  { name: 'Facebook', img: 'icon-facebook-icon', link: '#' },
-  { name: 'Instagram', img: 'icon-instagram-icon', link: '#' },
-  { name: 'Vk', img: 'icon-vk-icon', link: '#' },
-  { name: 'Viber', img: 'icon-viber-icon', link: '#' },
-  { name: 'Whatsapp', img: 'icon-whatsapp-icon', link: '#' },
+  {
+    name: 'Facebook',
+    img: 'icon-facebook-icon',
+    link: 'https://www.facebook.com/hotelstrigino',
+  },
+  {
+    name: 'Instagram',
+    img: 'icon-instagram-icon',
+    link: 'https://instagram.com/strigino_restaurant_hotel?utm_medium=copy_link',
+  },
+  {
+    name: 'Viber',
+    img: 'icon-viber-icon',
+    link: 'viber://add?number=79990763646',
+  },
+  {
+    name: 'Whatsapp',
+    img: 'icon-whatsapp-icon',
+    link: 'https://api.whatsapp.com/send?phone=79990763646',
+  },
   { name: 'Telegram', img: 'icon-telegram-icon', link: '#' },
 ];
 
