774fa296b780f8ebcf3102dc98531208c1b07eb5
Vladimir Liankevich <liankevichvladimir@gmail.com>
2022-06-01 15:17:16 +0300

    modify menu component and menu data

--- src/pages/reservation-restaurant-page/ReservationRestaurantPage.tsx
+++ src/pages/reservation-restaurant-page/ReservationRestaurantPage.tsx
@@ -7,6 +7,7 @@ import { getRestaurantData } from '../../data/restaurantData';
 import FormReservation from '../../components/formReservation/FormReservation';
 import ModalWindow from '../../components/common/modalWindow/ModalWindow';
 import { useState } from 'react';
+import Border from '../../assets/icons/other/borderMenu.png';
 
 const ReservationRestaurantPage = () => {
   const [modalShow, setModalShow] = useState<boolean>(false);
@@ -32,39 +33,27 @@ const ReservationRestaurantPage = () => {
         <ul className="reservation-restaurant-page__gifts reservation-restaurant-page__gifts_md reservation-restaurant-page__gifts_pd ">
           {giftsContent}
         </ul>
-        <h2 className="title title_fs28 title_align-start">Форма заказа</h2>
-        <div className="reservation-restaurant-page__wrapper">
-          <FormReservation />
-          <div className="reservation-restaurant-page__row">
-            <div className="reservation-restaurant-page__contact">
-              <div className="reservation-restaurant-page__info">
-                <div className="reservation-restaurant-page__contactTitle">
-                  ОРГАНИЗАЦИЯ МЕРОПРИЯТИЙ
-                </div>
 
-                <a
-                  className="reservation-restaurant-page__item"
-                  href="tel:+78312691612"
-                  title="+7 (831) 269-16-12"
-                >
-                  +7(831) 269-16-12
-                </a>
-                <a
-                  className="reservation-restaurant-page__item"
-                  href="mailto:hotelstrigino@mail.ru"
-                  title="hotelstrigino@mail.ru"
-                >
-                  hotelstrigino@mail.ru
-                </a>
-              </div>
-            </div>
+        <div className="reservation-restaurant-page__wrapper">
+          <div className="reservation-restaurant-page__row">
             <div
               className="reservation-restaurant-page__menu"
               onClick={() => setModalShow(true)}
             >
-              Меню
+              <img
+                src={Border}
+                alt="borderMenu"
+                className="reservation-restaurant-page__menu-border"
+              />
+              <div className="reservation-restaurant-page__menu-title">
+                Меню
+              </div>
             </div>
             <ModalWindow state={modalShow} setModalShow={setModalShow} />
+          </div>
+          <div>
+            <h2 className="title title_fs28 title_align-start">Форма заказа</h2>
+            <FormReservation />
           </div>
         </div>
       </div>
