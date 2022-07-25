7c0c0b8c2db3dace27c434ecfc6f9f65b9ac6700
Vladimir Liankevich <liankevichvladimir@gmail.com>
2022-07-25 12:47:49 +0300

    change sitemap.xml

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,18 +5,18 @@ import 'slick-carousel/slick/slick-theme.css';
 import { Header } from '../header/Header';
 import { Footer } from '../footer/Footer';
 import { Routers } from '../../route/Routers';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { getSocialsData } from '../../data/socialData';
 
 function App() {
   const socials = getSocialsData();
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Header socials={socials} />
         <Routers />
         <Footer socials={socials} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
